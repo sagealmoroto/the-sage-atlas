a296ad0ee315cfbb9fd77ac0b44b40c76e44f4d8
Riley Paige <167823142+sirentragedy@users.noreply.github.com>
2025-01-15 18:03:26 -0800

    slides .js

---
+++ main.js
@@ -0,0 +1,19 @@
+let index = 0; // Current slide
+const slides = document.querySelectorAll(".carousel");
+
+function showSlide(n) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === n ? "block" : "none";
+    });
+}
+
+// Next/previous controls
+function changeSlides(n) {
+    index +=n;
+    if (index >=slides.length) index = 0;
+    if (index < 0) index = slides.length - 1;
+    showSlide(index);
+}
+
+// Initial display
+showSlide(index);
