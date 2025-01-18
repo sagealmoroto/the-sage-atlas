2be7b1d292d85e39d091637fd4374142a0e72c4f
Riley Paige <167823142+sirentragedy@users.noreply.github.com>
2025-01-17 18:33:34 -0800

    adds card

---
+++ card.js
@@ -0,0 +1,14 @@
+const cardData = [
+    { className: "theTourist", title: "The Tourist" },
+    { className: "theFoodie", title: "The Foodie" },
+    { className: "theCultured", title: "The Cultured" },
+    { className: "thePartier", title: "The Partier" },
+    { className: "theAdventurer", title: "The Adventurer" },
+    { className: "theExplorer", title: "The Explorer" },
+    { className: "theRelaxer", title: "The Relaxer" },
+    { className: "theWell", title: "The Wellness Devotee" },
+    { className: "thePhotographer", title: "The Photographer" },
+    { className: "theNaturalist", title: "The Naturalist" },
+  ];
+
+  
