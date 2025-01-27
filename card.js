0fb3952d5278ca276fdfd6ce4c9cef74bf8458a4
Riley Paige <167823142+sirentragedy@users.noreply.github.com>
2025-01-27 13:44:08 -0800

    Removing card row divs

--- card.js
+++ card.js
@@ -49,35 +49,3 @@ fetch('cardData.json')
     });
   })
   .catch(error => console.error("Error loading JSON:", error));
-
-
-
-//     // and give it some content
-//     const newImg = document.createElement("img");
-//     newImg.setAttribute('src', currCard.imgSrc);
-//     newImg.setAttribute('alt', 'oops');
-//     newImg.style.width = '300px';
-//     newImg.style.height = '400px';
-
-//     // add the img node to the newly created div
-//     cardDiv.appendChild(newImg);
-
-//     // and give it some more content
-//     const newCountry = document.createElement("h4");
-//     newCountry.textContent = currCard.country;
-//     cardDiv.appendChild(newCountry);
-
-//     // add the newly created card to card container
-//     cardContainer.appendChild(cardDiv);
-// });
-
-    // "The Tourist": ".tourist-container",
-    //   "The Foodie": ".foodie-container",
-    //   "The Culture-Seeker": ".culture-container",
-    //   "The Partier": ".partier-container",
-    //   "The Adventurer": ".adventurer-container",
-    //   "The Explorer": ".explorer-container",
-    //   "The Relaxer": ".relaxer-container",
-    //   "The Wellness Devotee": ".wellness-container",
-    //   "The Photographer": ".photographer-container",
-    //   "The Naturalist": ".naturalist-container"
